4fd7b5a7f1eb5475a6d39ec27a5394576a820e5a
Arnab M <Arnab M>
2016-12-16 10:06:29 -0500

    End Chapter13

--- public/app.jsx
+++ public/app.jsx
@@ -1,24 +1,51 @@
   var Greeter = React.createClass ({
       getDefaultProps: function(){
           return{
-            name: 'React'
+            name: 'React',
+            message: 'This is the default message from React Component.'
           };
       },
+      getInitialState: function(){
+          return {
+              name: this.props.name 
+          };
+      },
+      onButtonClick: function(e){
+          e.preventDefault();
+          var nameRef = this.refs.name ;
+          name = nameRef.value ;
+          nameRef.value = '';
+
+          if (typeof name === 'string' && name.length > 0 ) {
+            this.setState ({
+                name:name
+            });
+          }
+      },
       render: function(){
-          var name = this.props.name ;
+          //var name = this.props.name ;
+          var name = this.state.name ;
+          var message = this.props.message ;
           return (
-              <div>
+        <div>
               <h1> Hello {name} ! </h1>
               <p>
-                This is from the React Component.
+                {message + '!!'}
               </p>
-              </div>
+            
+
+              <form onSubmit={this.onButtonClick}>
+                <input type="text" ref="name" />
+                <button> Set Name </button>
+              </form>
+
+         </div>
           );
       }
   });
   
   var firstName = "Arnab";
   ReactDOM.render(
-        <Greeter name={firstName}/> , document.getElementById('app')
+        <Greeter name={firstName} /> , document.getElementById('app')
     );
  //   chaper 11
